f82f7a29e7b253cfb10898fbd92242e45b379586
Wes Johnson <wes@swj.io>
2024-02-17 20:58:07 -0500

    init w/ reports to date

---
+++ README.md
@@ -0,0 +1,17 @@
+# unocha-flashes
+
+This repo contains the Flash Updates issued by the UN's Office for the Coordination of Humanitarian Affairs (OCHA). They've been converted from HTML into Markdown for easier parseability.
+
+The reports were daily except for the following reports which consolidated information for hiatus days:
+
+- 2023-12-18 covered two days (17th and 18th) - 1 skipped
+- 2023-12-26 covered four days (23rd to 26th) - 3 skipped
+- 2024-01-02 covered three days (Dec 31st to Jan 2nd) - 2 skipped
+- 2024-01-07 covered two days (6th and 7th) - 1 skipped
+- 2024-01-14 covered two days (13th and 14th) - 1 skipped
+- 2024-01-21 covered two days (20th and 21st) - 1 skipped
+- 2024-01-28 covered two days (27th and 28th) - 1 skipped
+- 2024-02-04 covered two days (3rd and 4th) - 1 skipped
+- 2024-02-12 covered three days (10th to 12th) - 2 skipped
+
+In total, 13 days will not have a report available.
---
+++ index.ts
@@ -0,0 +1,40 @@
+import fs from "fs";
+import puppeteer from "puppeteer";
+import { NodeHtmlMarkdown } from "node-html-markdown";
+import reportLinks from "./links.json";
+
+const getReport = async (slug: string) => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto(`https://www.ochaopt.org/${slug}`);
+
+  // Set screen size
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Wait and click on first result
+  const reportTimeBox = ".l-field-publish-date";
+  await page.waitForSelector(reportTimeBox);
+
+  // Locate the full title with a unique string
+  const timeNode = await page.waitForSelector(`${reportTimeBox} time`);
+  const isoTime = await timeNode?.evaluate((el) => el.getAttribute("datetime"));
+
+  console.log('Report datetime "%s".', isoTime);
+
+  const content = await page.waitForSelector(".container .content .layout");
+  const contentHtml = await content?.evaluate((el) => el.innerHTML);
+
+  await browser.close();
+
+  const markdown = NodeHtmlMarkdown.translate(/* html */ contentHtml);
+  fs.writeFileSync(`reports/flash-${isoTime}.md`, markdown);
+};
+
+reportLinks.reduce(async (chain, [linkText, href]) => {
+  await chain;
+  console.log(`fetching ${linkText} at ${href}`);
+  await getReport(href);
+}, Promise.resolve());
