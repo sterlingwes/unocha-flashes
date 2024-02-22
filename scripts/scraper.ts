e9fe3fbd50e2f2a5ebcc6e3461b3d253f24e9204
Wes Johnson <wes@swj.io>
2024-02-22 18:55:01 -0500

    provide status for fetch in link list

--- scripts/scraper.ts
+++ scripts/scraper.ts
@@ -3,6 +3,11 @@ import puppeteer from "puppeteer";
 import { NodeHtmlMarkdown } from "node-html-markdown";
 import reportLinks from "./links.json";
 
+// @ts-expect-error forcing type
+const unfetchedLinks: [string, string, boolean][] = reportLinks.filter(
+  ([, , fetched]) => !fetched
+);
+
 const getReport = async (slug: string) => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
@@ -33,7 +38,8 @@ const getReport = async (slug: string) => {
   fs.writeFileSync(`reports/flash-${isoTime}.md`, markdown);
 };
 
-reportLinks.reduce(async (chain, [linkText, href]) => {
+console.log(`fetching from ${unfetchedLinks.length} unfetched links`);
+unfetchedLinks.reduce(async (chain, [linkText, href]) => {
   await chain;
   console.log(`fetching ${linkText} at ${href}`);
   await getReport(href);
